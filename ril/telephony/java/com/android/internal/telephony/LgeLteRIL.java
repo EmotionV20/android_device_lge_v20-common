1d45f9db651c6316595c51abbeb95b7557251761
Mike <calmdownkid13@gmail.com>
2017-01-23 01:21:13 -0500

    Update LgeLteRIL.java

--- ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
@@ -19,7 +19,6 @@ package com.android.internal.telephony;
 import static com.android.internal.telephony.RILConstants.*;
 
 import android.content.Context;
-import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
 
@@ -33,6 +32,7 @@ public class LgeLteRIL extends RIL implements CommandsInterface {
     public static final int RIL_UNSOL_AVAILABLE_RAT = 1054;
     public static final int RIL_UNSOL_LOG_RF_BAND_INFO = 1165;
     public static final int RIL_UNSOL_LTE_REJECT_CAUSE = 1187;
+
     public LgeLteRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
         super(context, preferredNetworkType, cdmaSubscription, null);
     }
@@ -42,32 +42,50 @@ public class LgeLteRIL extends RIL implements CommandsInterface {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
     }
 
-    @Override
-    protected Object
-    responseFailCause(Parcel p) {
-        int numInts;
-        int response[];
+    static String
+    lgeResponseToString(int request)
+    {
+        switch(request) {
+            case RIL_UNSOL_AVAILABLE_RAT: return "RIL_UNSOL_AVAILABLE_RAT";
+            case RIL_UNSOL_LOG_RF_BAND_INFO: return "RIL_UNSOL_LOG_RF_BAND_INFO";
+            case RIL_UNSOL_LTE_REJECT_CAUSE: return "RIL_UNSOL_LTE_REJECT_CAUSE";
+            default: return "<unknown response>";
+        }
+    }
 
-        numInts = p.readInt();
-        response = new int[numInts];
-        for (int i = 0 ; i < numInts ; i++) {
-            response[i] = p.readInt();
-        }
-        LastCallFailCause failCause = new LastCallFailCause();
-        failCause.causeCode = response[0];
-        if (p.dataAvail() > 0) {
-          failCause.vendorCause = p.readString();
-        }
-        return failCause;
+    protected void lgeUnsljLogRet(int response, Object ret) {
+        riljLog("[LGE-UNSL]< " + lgeResponseToString(response) + " " + retToString(response, ret));
     }
 
     @Override
-    public void getRadioCapability(Message response) {
-        riljLog("getRadioCapability: returning static radio capability");
-        if (response != null) {
-            Object ret = makeStaticRadioCapability();
-            AsyncResult.forMessage(response, ret, null);
-            response.sendToTarget();
+    protected void
+    processUnsolicited (Parcel p, int type) {
+        Object ret;
+        int dataPosition = p.dataPosition(); // save off position within the Parcel
+        int response = p.readInt();
+
+        switch(response) {
+            case RIL_UNSOL_AVAILABLE_RAT: ret = responseInts(p); break;
+            case RIL_UNSOL_LOG_RF_BAND_INFO: ret = responseInts(p); break;
+            case RIL_UNSOL_LTE_REJECT_CAUSE: ret = responseInts(p); break;
+            default:
+                // Rewind the Parcel
+                p.setDataPosition(dataPosition);
+                // Forward responses that we are not overriding to the super class
+                super.processUnsolicited(p, type);
+                return;
+        }
+
+        switch(response) {
+            case RIL_UNSOL_AVAILABLE_RAT:
+                if (RILJ_LOGD) lgeUnsljLogRet(response, ret);
+                break;
+            case RIL_UNSOL_LOG_RF_BAND_INFO:
+                if (RILJ_LOGD) lgeUnsljLogRet(response, ret);
+                break;
+            case RIL_UNSOL_LTE_REJECT_CAUSE:
+                if (RILJ_LOGD) lgeUnsljLogRet(response, ret);
+                break;
         }
     }
 
